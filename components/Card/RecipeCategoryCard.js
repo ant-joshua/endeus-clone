d581943ddf65718df69b9f60cf9164a74dc6a7b3
keva damar galih <kevadamarg@gmail.com>
2021-10-02 22:31:57 +0700

    FEAT: Change UI Recipe Category Card

--- components/Card/RecipeCategoryCard.js
+++ components/Card/RecipeCategoryCard.js
@@ -1,20 +1,19 @@
-import classnames from "classnames";
+import classnames from 'classnames';
 export default function RecipeCategoryCard(props) {
+  const { title, img } = props;
+
   return (
     <>
       <a className={classnames(props.className)}>
         <div className="rounded-lg bg-white shadow-lg">
           <figure className="relative flex flex-col">
             <picture className="w-full h-full flex justify-center items-center">
-              <img
-                src="https://img.kurio.network/qQ1Q4CkWVe1xzU29-FGrgv2kHQE=/200x200/filters:quality(80)/https://kurio-img.kurioapps.com/20/10/15/c27cd268-b22e-4a3e-93f7-009e7f100696.png"
-                className="object-contain"
-              />
+              <img src={img} className="object-contain" />
             </picture>
-            <figcaption className="hidden">Resep Ayam</figcaption>
+            <figcaption className="hidden">{title}</figcaption>
           </figure>
         </div>
-        <p className="text-center mt-1 text-gray-500 text-sm">Resep Ayam</p>
+        <p className="text-center mt-1 text-gray-500 text-sm">{title}</p>
       </a>
     </>
   );
